65e42b3ccd6aa5f0a7e2ebc972925cbe73235379
Hwangjae <vuhoan@sheridancollege.ca>
2021-02-23 20:31:52 -0500

    Added password validator as a class

---
+++ src/midtermreviewcodeforpartc/PasswordValidator.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package midtermreviewcodeforpartc;
+import java.util.Scanner;
+/**
+ *
+ * @author Hoang Vu <vuhoan@sheridancollege.ca> <andrewvu270@gmail.com>
+ */
+public class PasswordValidator {
+    
+    public static void isValid(String password){
+        boolean validPassword = false;
+        while(!validPassword){
+            int specialCharCount = 0;
+            for(int i=0;i<password.length(); i++)
+            {
+                if(!(Character.isLetterOrDigit(password.charAt(i))))
+                {
+                    //now we know there is at least one special character
+                    specialCharCount++;
+                }
+            }
+            if(specialCharCount>0 &&password.length()>7)
+            {
+                validPassword = true;
+                break;
+            }
+            
+            System.out.println("Passwords must have at least 8 characters");
+            System.out.println("Passwords must have at least one special character");
+            System.out.println("Please enter your desired password:"); 
+            Scanner sc = new Scanner(System.in);
+            password = sc.nextLine();
+            
+            //iterate over each character to see if it is a special character
+            
+        }
+    }
+}
--- src/midtermreviewcodeforpartc/UnoOnline.java
+++ src/midtermreviewcodeforpartc/UnoOnline.java
@@ -42,30 +42,11 @@ public class UnoOnline
         Scanner sc = new Scanner(System.in);
         System.out.println("please enter your desired user name:");
         String userName = sc.nextLine();
-        boolean validPassword=false;
-        String password="";
-        while(!validPassword)
-        {
-            System.out.println("Passwords must have at least 8 characters");
-            System.out.println("Passwords must have at least one special character");
-            System.out.println("Please enter your desired password:"); 
-            
-            password = sc.nextLine();
-            int specialCharCount=0;
-            //iterate over each character to see if it is a special character
-            for(int i=0;i<password.length(); i++)
-            {
-                if(!(Character.isLetterOrDigit(password.charAt(i))))
-                {
-                    //now we know there is at least one special character
-                    specialCharCount++;
-                }
-            }
-            if(specialCharCount>0 &&password.length()>7)
-            {
-                validPassword=true;
-            }
-        }//loop only ends when password is valid so now we create the User
+        System.out.println("Passwords must have at least 8 characters");
+        System.out.println("Passwords must have at least one special character");
+        System.out.println("Please enter your desired password:"); 
+        String password = sc.nextLine();
+        PasswordValidator.isValid(password);
         
         User newUser = new User(userName, password);
         users[userCount] = newUser;//add the new user to the user list
@@ -74,6 +55,6 @@ public class UnoOnline
         System.out.println("UserName: " + userName);
         System.out.println("Password: just kidding can't show password");
     }//end run method
-   
+    
     
 }//end class
